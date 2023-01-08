556a8585c8d85e01f4bf37a20c267999d883f845
MKHLink <[likhonhasankomol@gmail.com]>
2023-01-08 15:21:49 -0500

    updated queries and mutations to work with the current server setup

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,63 +1,79 @@
 import {gql} from '@apollo/client';
 
 export const ADD_WORKOUT = gql`
-  mutation AddWorkout(
-    $workoutName: String!
-    $workoutType: String!
-    $calsBurned: String
-    $time: String
-    $notes: String
-  ) {
-    addWorkout(
-      workoutName: $workoutName
-      workoutType: $workoutType
-      calsBurned: $calsBurned
-      time: $time
-      notes: $notes
-    ) {
+$workoutType: String!, $calsBurned: String, $time: String, $notes: String, $gymLocation: String) {
+  addWorkout(workoutName: $workoutName, workoutType: $workoutType, calsBurned: $calsBurned, time: $time, notes: $notes, gymLocation: $gymLocation) {
+    _id
+    createdAt
+    workoutName
+    username
+    workoutType
+    calsBurned
+    time
+    notes
+    gymLocation
+    reactions {
+      _id
+      comment
+      reactionType
       username
-      workoutName
-      workoutType
-      calsBurned
-      time
-      createdAt
-      notes
     }
+    reactionCount
   }
+}
 `;
 
 export const LOGIN = gql`
-  mutation Login($email: String!, $password: String!) {
-    userLogin(email: $email, password: $password) {
-      token
+mutation UserLogin($email: String!, $password: String!) {
+  userLogin(email: $email, password: $password) {
+    token
+    user {
+      _id
+      username
+      email
+      firstName
+      lastName
+      age
+      status
+      expLevel
+      workouts {
+        gymLocation
+        _id
+        createdAt
+        workoutName
+        username
+        workoutType
+        calsBurned
+        time
+        notes
+        reactions {
+          _id
+          comment
+          reactionType
+          username
+        }
+        reactionCount
+      }
+      gym
     }
   }
+}
 `;
 
 export const SIGNUP = gql`
-  mutation AddUser(
-    $username: String!
-    $email: String!
-    $password: String!
-    $firstName: String!
-    $lastName: String!
-    $status: String!
-    $expLevel: String!
-    $gym: String!
-  ) {
-    addUser(
-      username: $username
-      email: $email
-      password: $password
-      firstName: $firstName
-      lastName: $lastName
-      status: $status
-      expLevel: $expLevel
-      gym: $gym
-    ) {
-      user {
-        lastName
-      }
+mutation AddUser($username: String!, $email: String!, $password: String!, $firstName: String!, $lastName: String!, $age: Int!, $status: String!, $expLevel: String!, $gym: String!) {
+  addUser(username: $username, email: $email, password: $password, firstName: $firstName, lastName: $lastName, age: $age, status: $status, expLevel: $expLevel, gym: $gym) {
+    user {
+      _id
+      username
+      email
+      firstName
+      lastName
+      age
+      status
+      expLevel
+      gym
     }
   }
+}
 `;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,13 +1,58 @@
 import { gql } from "@apollo/client";
 
 export const GET_WORKOUTS = gql`
-  query GetWorkouts {
-    getWorkouts {
+query GetWorkouts($username: String) {
+  getWorkouts(username: $username) {
+    _id
+    createdAt
+    workoutName
+    username
+    workoutType
+    calsBurned
+    time
+    notes
+    gymLocation
+    reactions {
+      _id
+      reactionType
+      comment
+      username
+    }
+    reactionCount
+  }
+}
+`;
+
+export const GET_ME = gql`
+query Me {
+  me {
+    _id
+    username
+    email
+    firstName
+    lastName
+    age
+    status
+    expLevel
+    workouts {
+      _id
+      createdAt
       workoutName
+      username
       workoutType
-      notes
       calsBurned
       time
+      notes
+      gymLocation
+      reactions {
+        _id
+        comment
+        reactionType
+        username
+      }
+      reactionCount
     }
+    gym
   }
+}
 `;
