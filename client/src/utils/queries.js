07b3293b6c0713650a4f7c013e930fdd0d8e713f
MKHLink <[likhonhasankomol@gmail.com]>
2023-01-08 15:02:58 -0500

    seperated queries and mutations in different file in utils dir, set up gql and jwt-decode as dependecies, set up an Auth file with a AuthService class that checks user token for authentication and decodes tokens for usr data

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,68 +1,6 @@
 import { gql } from "@apollo/client";
 
-export const SIGNUP = gql`
-  mutation AddUser(
-    $username: String!
-    $email: String!
-    $password: String!
-    $firstName: String!
-    $lastName: String!
-    $status: String!
-    $expLevel: String!
-    $gym: String!
-  ) {
-    addUser(
-      username: $username
-      email: $email
-      password: $password
-      firstName: $firstName
-      lastName: $lastName
-      status: $status
-      expLevel: $expLevel
-      gym: $gym
-    ) {
-      user {
-        lastName
-      }
-    }
-  }
-`;
-
-export const LOGIN = gql`
-  mutation Login($email: String!, $password: String!) {
-    userLogin(email: $email, password: $password) {
-      token
-    }
-  }
-`;
-
-export const WORKOUT = gql`
-  mutation AddWorkout(
-    $workoutName: String!
-    $workoutType: String!
-    $calsBurned: String
-    $time: String
-    $notes: String
-  ) {
-    addWorkout(
-      workoutName: $workoutName
-      workoutType: $workoutType
-      calsBurned: $calsBurned
-      time: $time
-      notes: $notes
-    ) {
-      username
-      workoutName
-      workoutType
-      calsBurned
-      time
-      createdAt
-      notes
-    }
-  }
-`;
-
-export const WORKOUTS = gql`
+export const GET_WORKOUTS = gql`
   query GetWorkouts {
     getWorkouts {
       workoutName
